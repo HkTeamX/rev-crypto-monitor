9cab9becc6d2793b9c778e99f97392f6a10521cf
huankong233 <huank0ng@foxmail.com>
2026-04-16 15:03:55 +0800

    [*] 修复编译报错

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,14 @@
-import antfu from "@antfu/eslint-config"
+import antfu from '@antfu/eslint-config'
 
-export default antfu()
+export default antfu(
+  {
+    type: 'app',
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      './src-tauri/target/**',
+      './src-tauri/gen/**',
+      '**/vite-env.d.ts',
+    ],
+  },
+)
